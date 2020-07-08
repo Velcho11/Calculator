a6ef6c7b900676f8c2070dda259522ceb869e836
Velcho11 <67707649+Velcho11@users.noreply.github.com>
2020-07-08 20:50:34 +0300

    Console tab2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,18 +1,34 @@
 import { Component } from '@angular/core';
-
+import { Storage } from '@ionic/storage';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
+
 export class Tab2Page {
+  private digits:{title:number}[]=[];
   result: number
   a: number = 0;
   b: number = 0;
-  constructor() {
+  key:string=" "
+  
+
+  constructor(private storage: Storage) {
     }
+
     onAdd(){
       this.result = this.a - this.b;
+      this.storage.set(this.key,this.a);
+      this.storage.set(this.key,this.b);
+      this.storage.set(this.key,this.result);
       }
-
-}
+      Allget(){
+this.storage.get(this.key).then((val)=>{
+console.log('First Number: ',this.a);
+console.log('Second Number:',this.b);
+console.log('Result:',this.result );
+})
+      }
+     }
+    
