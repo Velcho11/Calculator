d8e3f79d36e4f37cb3aebb37212abd2db633ca12
Anastasiy02 <67695301+Anastasiy02@users.noreply.github.com>
2020-07-09 11:18:09 +0300

    Tab3 Storage

--- src/app/tab3/tab3-routing.module.ts
+++ src/app/tab3/tab3-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Tab3Page } from './tab3.page';
-
+import { Storage } from '@ionic/storage';
 const routes: Routes = [
   {
     path: '',
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Storage } from '@ionic/storage';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -10,10 +10,20 @@ export class Tab3Page {
   result: number
   c: number = 0;
   d: number = 0;
-  constructor() {
+  key:string = "";
+  constructor(private storage: Storage) {
     }
     onAdd(){
       this.result = this.c * this.d;
+      this.storage.set(this.key,this.c);
+      this.storage.set(this.key,this.d);
+      this.storage.set(this.key,this.result);
       }
-
+      Allget(){
+        this.storage.get(this.key).then((val)=>{
+        console.log('First Number: ',this.c);
+        console.log('Second Number:',this.d);
+        console.log('Result:',this.result )
+        })
+              }
 }
